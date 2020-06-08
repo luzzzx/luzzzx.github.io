2e16ddd72e0b466e9f50e387402565b54cab197f
luzzzx <lu.vldmr@gmail.com>
2020-06-08 21:29:37 +0300

    test

---
+++ lion-test/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $("#pagepiling").pagepiling({
+    menu: null,
+    direction: "horizontal",
+    verticalCentered: true,
+    sectionsColor: [],
+    anchors: [],
+    scrollingSpeed: 700,
+    easing: "swing",
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: false,
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: ".section",
+    animateAnchor: false,
+
+    //events
+    onLeave: function (index, nextIndex, direction) {},
+    afterLoad: function (anchorLink, index) {},
+    afterRender: function () {}
+  });
+});

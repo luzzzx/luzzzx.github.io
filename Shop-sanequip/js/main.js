c3067aa443f7595f8a59b81e90b5582c8c7b6b1a
luzzzx <lu.vldmr@gmail.com>
2020-06-27 21:27:31 +0300

    new

--- Shop-sanequip/js/main.js
+++ Shop-sanequip/js/main.js
@@ -4,4 +4,49 @@ $(document).ready(function () {
         items: 1,
         nav: true
     });
+    $('.product-slider-body').owlCarousel({
+        items: 1,
+        loop: false,
+        center: true,
+        nav: true,
+        URLhashListener: true,
+        autoplayHoverPause: true,
+        startPosition: 'URLHash'
+    });
+    // plus minus
+    jQuery(function () {
+        var j = jQuery; //Just a variable for using jQuery without conflicts
+        var addInput = '#qty'; //This is the id of the input you are changing
+        var n = 1; //n is equal to 1
+
+        //Set default value to n (n = 1)
+        j(addInput).val(n);
+
+        //On click add 1 to n
+        j('.plus').on('click', function () {
+            j(addInput).val(++n);
+        })
+
+        j('.min').on('click', function () {
+            //If n is bigger or equal to 1 subtract 1 from n
+            if (n >= 1) {
+                j(addInput).val(--n);
+            } else {
+                //Otherwise do nothing
+            }
+        });
+    });
+
+    $(".tabs-item").on("click", function () {
+        let $this = $(this),
+            i = $this.data("id");
+        $this.addClass("active").siblings().removeClass("active");
+        $this
+            .closest('.tabs')
+            .find(".tab-main [data-id=" + i + "]")
+            .addClass("active")
+            .siblings()
+            .removeClass("active");
+    });
 });
+

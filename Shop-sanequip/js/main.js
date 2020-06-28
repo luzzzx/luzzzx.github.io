05a5ab6f62db1a7374da022f330e23231106a96c
luzzzx <lu.vldmr@gmail.com>
2020-06-28 18:33:57 +0300

    1

--- Shop-sanequip/js/main.js
+++ Shop-sanequip/js/main.js
@@ -14,27 +14,42 @@ $(document).ready(function () {
         startPosition: 'URLHash'
     });
     // plus minus
-    jQuery(function () {
-        var j = jQuery; //Just a variable for using jQuery without conflicts
-        var addInput = '#qty'; //This is the id of the input you are changing
-        var n = 1; //n is equal to 1
+    // jQuery(function () {
+    //     var j = jQuery; //Just a variable for using jQuery without conflicts
+    //     var addInput = '#qty'; //This is the id of the input you are changing
+    //     var n = 1; //n is equal to 1
 
-        //Set default value to n (n = 1)
-        j(addInput).val(n);
+    //     //Set default value to n (n = 1)
+    //     j(addInput).val(n);
 
-        //On click add 1 to n
-        j('.plus').on('click', function () {
-            j(addInput).val(++n);
-        })
+    //     //On click add 1 to n
+    //     j('.plus').on('click', function () {
+    //         j(addInput).val(++n);
+    //     })
 
-        j('.min').on('click', function () {
-            //If n is bigger or equal to 1 subtract 1 from n
-            if (n >= 1) {
-                j(addInput).val(--n);
-            } else {
-                //Otherwise do nothing
-            }
-        });
+    //     j('.min').on('click', function () {
+    //         //If n is bigger or equal to 1 subtract 1 from n
+    //         if (n >= 1) {
+    //             j(addInput).val(--n);
+    //         } else {
+    //             //Otherwise do nothing
+    //         }
+    //     });
+    // });
+
+    $('.min').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
     });
 
     $(".tabs-item").on("click", function () {

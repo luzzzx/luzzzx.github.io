3142c8a44516747e455d047aab06a25be6e481c6
luzzzx <lu.vldmr@gmail.com>
2020-08-07 18:53:23 +0300

    123

--- landCrm/js/script.js
+++ landCrm/js/script.js
@@ -2,34 +2,34 @@ $(document).scroll(function () {
     var $nav = $(".fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
-$(document).ready(function(){
-    $('.burger-menu').on( "click", function() {
+$(document).ready(function () {
+    $('.burger-menu').on("click", function () {
         $('.burger-menu').toggleClass('active');
-      });
+    });
     $(".slider-sert").owlCarousel({
-        loop:true,
-        margin:30,
-        responsive : {
+        loop: true,
+        margin: 30,
+        responsive: {
             // breakpoint from 0 up
-            0 : {
-                items:1,
-                nav:false,
+            0: {
+                items: 1,
+                nav: true,
             },
             // breakpoint from 480 up
-            480 : {
-                items:2,
-                nav:false,
+            480: {
+                items: 2,
+                nav: false,
             },
             // breakpoint from 768 up
-            768 : {
-                items:4,
-                nav:true,
+            768: {
+                items: 4,
+                nav: true,
             }
         }
     });
     $(".comment-slider").owlCarousel({
-        loop:true,
-        items:1,
-        nav:true,
+        loop: true,
+        items: 1,
+        nav: true,
     });
-  });
+});

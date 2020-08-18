a47db341a23c84c58988fda806e7191687ffe1d4
luzzzx <lu.vldmr@gmail.com>
2020-08-18 18:37:16 +0300

    2

--- dashB/js/main.js
+++ dashB/js/main.js
@@ -6,11 +6,6 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
-// document.querySelector('.custom-file-input').addEventListener('change', function (e) {
-//     var fileName = document.getElementById("myInput").files[0].name;
-//     var nextSibling = e.target.nextElementSibling
-//     nextSibling.innerText = fileName
-// })
 
 $('.custom-file input').change(function (e) {
     if (e.target.files.length) {
@@ -18,6 +13,26 @@ $('.custom-file input').change(function (e) {
     }
 });
 
-// $('.clear-form-file').click(function () {
-//     $('.custom-file-label').input.value = '';
-// })
+function chBoxDel() {
+    var chbox;
+    chbox = document.getElementById('HideDelisted');
+    if (chbox.checked) {
+        $('.card-name:contains("Delisted")').hide()
+        $('.card-name:contains("Delisted")').next().hide()
+
+    }
+    else {
+        $('.card-name:contains("Delisted")').show()
+        $('.card-name:contains("Delisted")').next().attr('style', '')
+    }
+}
+$(function () {
+    $('#GenesisKnwoledge ').datepicker();
+});
+// let delist = $("#HideDelisted");
+// if (delist.prop("checked")) {
+//     console.log('hi');
+// }
+// $(".card-name").text().indexOf('Delisted').hide();
+
+// $(".card-name").find($('.name-status')).text()

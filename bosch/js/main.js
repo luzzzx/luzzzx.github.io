6faa99cb20866bfaa67928acf0c87659ab379206
luzzzx <lu.vldmr@gmail.com>
2020-09-05 18:51:47 +0300

    12

--- bosch/js/main.js
+++ bosch/js/main.js
@@ -31,15 +31,31 @@ $('.product-related__row').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 4.5,
     dots: false,
     slidesToScroll: 1,
     responsive: [
+        {
+            breakpoint: 1253,
+            settings: {
+                arrows: false,
+                slidesToShow: 3.5,
+                slidesToScroll: 1,
+            }
+        },
         {
             breakpoint: 769,
             settings: {
                 arrows: false,
                 slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 683,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
                 slidesToScroll: 1,
             }
         }
@@ -50,10 +66,18 @@ $('.recently-viewed__row').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 4.5,
     dots: false,
     slidesToScroll: 1,
     responsive: [
+        {
+            breakpoint: 1253,
+            settings: {
+                arrows: false,
+                slidesToShow: 3.5,
+                slidesToScroll: 1,
+            }
+        },
         {
             breakpoint: 769,
             settings: {
@@ -61,6 +85,22 @@ $('.recently-viewed__row').slick({
                 slidesToShow: 3,
                 slidesToScroll: 1,
             }
+        },
+        {
+            breakpoint: 683,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
         }
     ]
+});
+
+
+$(".togler").click(function () {
+    $(this).siblings('menu').slideToggle("slow", function () {
+        // Animation complete.
+    });
+    $(this).toggleClass('active');
 });

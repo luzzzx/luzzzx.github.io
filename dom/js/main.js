ae2da38f1c77544ad68ba9f6387f7300ad1db1f0
luzzzx <lu.vldmr@gmail.com>
2020-10-02 11:33:50 +0300

    123

--- dom/js/main.js
+++ dom/js/main.js
@@ -2,12 +2,17 @@ $(document).ready(function () {
     $('.burger').on("click", function () {
         $('.burger').toggleClass('active');
         $('.main-menu').toggleClass('active');
+        $('body').append('<div class="modal-backdrop fade show"></div>');
     });
     $('.close-menu').on("click", function () {
         $('.burger').removeClass('active');
         $('.main-menu').removeClass('active');
+        $(".modal-backdrop").remove();
     });
     $('.user').on("click", function () {
         $('.drop-down__menu').toggleClass('active');
+    });
+    $('.anchor-top').on("click", function () {
+        $(window).scrollTop(0);
     });
 });

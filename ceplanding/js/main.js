f281b0f46a6a68248a1a175df3f5235558019695
luzzzx <lu.vldmr@gmail.com>
2020-11-24 21:55:19 +0200

    project-page

--- ceplanding/js/main.js
+++ ceplanding/js/main.js
@@ -34,6 +34,19 @@ $(document).ready(function () {
         "mask": "+7(999) 999-99-99",
         showMaskOnHover: false
     })
+
+    // model maket
+    $('#cep-1').on("click", function () {
+        $('#cep-1-modal').toggleClass('active');
+        $('.model-main').toggleClass('active');
+    });
+    
+    $('#cep-2').on("click", function () {
+        $('#cep-2-modal').toggleClass('active');
+        $('.model-main').toggleClass('active');
+    });
+    // model maket
+    
     // anim
     particlesJS("particles-js", {
         "particles": {
@@ -145,27 +158,13 @@ $(document).ready(function () {
         },
         "retina_detect": true
     });
-    var count_particles, stats, update;
-    stats = new Stats;
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
-    count_particles = document.querySelector('.js-count-particles');
-    update = function() {
-        stats.begin();
-        stats.end();
-        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-            count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-        }
-        requestAnimationFrame(update);
-    };
-    requestAnimationFrame(update);;
 
     // anim
 
     // mask
+
+    // model maket
+
 
 
 });

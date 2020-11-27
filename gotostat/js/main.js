479d3c305ae81fec4b21410805ac2a6d903de4ba
luzzzx <lu.vldmr@gmail.com>
2020-11-28 00:55:04 +0200

    1st
    1st

---
+++ gotostat/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+
+    if ($(window).width() < 1350) {
+        $('.drp-d a').on("click", function () {
+            $(this).next('.drp-d-menu').slideToggle();
+        });
+        $('.drp-d-2 a').on("click", function () {
+            $(this).next('.submenu').slideToggle();
+        });
+        $('.burger-menu').on("click", function () {
+            $(this).toggleClass('active');
+            $('.main-menu').toggleClass('active');
+            $('body').toggleClass('lock');
+        });
+
+	}
+	
+
+});

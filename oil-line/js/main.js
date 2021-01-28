6aef83a26a3cd61030c6bd4bc80eaedc61228d5a
luzzzx <lu.vldmr@gmail.com>
2021-01-28 19:28:54 +0200

    4
    4

--- oil-line/js/main.js
+++ oil-line/js/main.js
@@ -226,4 +226,15 @@ $('.product-slider__thumb').slick({
   focusOnSelect: true
 });
 
+
+// basket quanity
+
+$('.btn-plus, .btn-minus').on('click', function(e) {
+  const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+  const input = $(e.target).closest('.input-group').find('input');
+  if (input.is('input')) {
+    input[0][isNegative ? 'stepDown' : 'stepUp']()
+  }
+})
+
 });

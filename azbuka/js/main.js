fc27f1e6c5c150b5be61bf668f01c88d50360482
luzzzx <lu.vldmr@gmail.com>
2021-02-24 21:08:58 +0200

    catalog
    catalog

--- azbuka/js/main.js
+++ azbuka/js/main.js
@@ -79,6 +79,27 @@ $('.sale-product-slider').slick({
   ]
 });
 
+$('.popular-product-slider').slick({
+  slidesToShow: 4,
+  arrows: true,
+  prevArrow: $('.popular-product-slider__prev'),
+  nextArrow: $('.popular-product-slider__next'),
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
 $('.season-product-slider').slick({
   slidesToShow: 3,
   arrows: true,
@@ -224,13 +245,6 @@ $(window).on('load resize', function () {
       // $('.road-slider__item:gt(2)').hide();
     }
   } 
-  // if ($(this).width() < 760){
-
-  //   if ($('.certificate-row__item').length > 2) {
-  //     $('.certificate-row__item:gt(1)').addClass('extra');
-  //     // $('.road-slider__item:gt(2)').hide();
-  //   }
-  // }
   else {
       $('.certificate-row__item').removeClass('extra');
   }
@@ -249,3 +263,687 @@ $(window).on('load resize', function () {
       $('.news-body').removeClass('extra');
   }
 })
+
+
+// list grid btns
+
+$("#column-prod").click(function(e){
+  e.preventDefault();
+  $(this).addClass('active');
+  $('.product-card').removeClass("list");
+  $('.product-card').removeClass("grid");
+  $('.catalog-product__main-item').removeClass("list");
+  $('.catalog-product__main-item').removeClass("grid");
+  $('#grid-prod').removeClass('active');
+  $('#list-prod').removeClass('active');
+  $('.product-card').addClass("column");
+  $('.catalog-product__main-item').addClass("column");
+});
+
+$("#grid-prod").click(function(e){
+  e.preventDefault();
+  $(this).addClass('active');
+  $('.product-card').removeClass("list");
+  $('.product-card').removeClass("column");
+  $('.catalog-product__main-item').removeClass("column");
+  $('.catalog-product__main-item').removeClass("list");
+  $('#list-prod').removeClass('active');
+  $('#column-prod').removeClass('active');
+  $('.product-card').addClass("grid");
+  $('.catalog-product__main-item').addClass("grid");
+});
+
+$("#list-prod").click(function(e){
+  e.preventDefault();
+  $(this).addClass('active');
+  $('.product-card').removeClass("grid");
+  $('.product-card').removeClass("column");
+  $('.catalog-product__main-item').removeClass("grid");
+  $('.catalog-product__main-item').removeClass("column");
+  $('#grid-prod').removeClass('active');
+  $('#column-prod').removeClass('active');
+  $('.product-card').addClass("list");
+  $('.catalog-product__main-item').addClass("list");
+});
+
+$(window).on('load resize', function () {
+  if ($(this).width() < 765) {
+    $('.catalog-product__main-item').removeClass("grid");
+    $('.catalog-product__main-item').removeClass("column");
+    $('.catalog-product__main-item').removeClass("list");
+    $('.product-card').removeClass("grid");
+    $('.product-card').removeClass("column");
+    $('.product-card').removeClass("list");
+    $('#grid-prod').removeClass('active');
+    $('#column-prod').removeClass('active');
+    $('#list-prod').removeClass('active');
+  } else{
+    $('.catalog-product__main-item').addClass("list");
+    $('#list-prod').addClass('active');
+    $('.product-card').addClass("list");
+  }
+})
+// catalog range
+
+
+
+function priceSlider(){
+  let $range = $("#price-range"),
+  $inputFrom = $(".price-range-from"),
+  $inputTo = $(".price-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+priceSlider();
+
+function widthSlider(){
+  let $range = $("#width-range"),
+  $inputFrom = $(".width-range-from"),
+  $inputTo = $(".width-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+widthSlider();
+
+function heightSlider(){
+  let $range = $("#height-range"),
+  $inputFrom = $(".height-range-from"),
+  $inputTo = $(".height-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+heightSlider();
+
+
+function deepSlider(){
+  let $range = $("#deep-range"),
+  $inputFrom = $(".deep-range-from"),
+  $inputTo = $(".deep-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+deepSlider();
+
+
+// range mobile
+
+function priceSliderMob(){
+  let $range = $("#price-range1"),
+  $inputFrom = $(".price-range-from"),
+  $inputTo = $(".price-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+priceSliderMob();
+
+function widthSliderMob(){
+  let $range = $("#width-range1"),
+  $inputFrom = $(".width-range-from"),
+  $inputTo = $(".width-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+widthSliderMob();
+
+function heightSliderMob(){
+  let $range = $("#height-range1"),
+  $inputFrom = $(".height-range-from"),
+  $inputTo = $(".height-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+heightSliderMob();
+
+
+function deepSliderMob(){
+  let $range = $("#deep-range1"),
+  $inputFrom = $(".deep-range-from"),
+  $inputTo = $(".deep-range-to"),
+  instance,
+  min = 0,
+  max = 180,
+  from = 0,
+  to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    grid: false,             // show/hide grid
+    drag_interval: true,
+    force_edges: true,     // force UI in the box
+    hide_min_max: true,    // show/hide MIN and MAX labels
+    hide_from_to: true,    // show/hide FROM and TO labels
+    block: false            // block instance from changing
+});
+
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+from = data.from;
+  to = data.to;
+  
+  $inputFrom.prop("value", from);
+  $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < min) {
+      val = min;
+  } else if (val > to) {
+      val = to;
+  }
+  
+  instance.update({
+      from: val
+  });
+});
+
+$inputTo.on("input", function () {
+  var val = $(this).prop("value");
+  
+  // validate
+  if (val < from) {
+      val = from;
+  } else if (val > max) {
+      val = max;
+  }
+  
+  instance.update({
+      to: val
+  });
+});
+
+}
+
+deepSliderMob();
+
+// range mobile
+
+// filter block 
+
+$(".filter-block__title.toggle").click(function(e){
+  e.preventDefault();
+  $(this).toggleClass('active').next().slideToggle();
+});
+
+
+// показать больше
+
+const txt = document.querySelector(".page-text__body-wrapper");
+const txtInitialHeight = txt.clientHeight;
+const overflow = document.querySelector(".overflow");
+const btn = document.querySelector(".toggle-more");
+
+btn.addEventListener("click", initToggle);
+
+function initToggle(e) {
+  e.preventDefault();
+  txt.style.maxHeight =
+    e.target.dataset.state === "more"
+      ? `${txt.scrollHeight}px`
+      : `${txtInitialHeight}px`;
+  e.target.setAttribute(
+    "data-state",
+    e.target.dataset.state === "more" ? "less" : "more"
+  );
+  e.target.innerHTML =
+    e.target.dataset.state === "more" ? "Подробнее" : "скрыть";
+  overflow.setAttribute(
+    "data-state",
+    e.target.dataset.state === "more" ? "visible" : "hidden"
+  );
+}
+
+
+// mobile filter block
+$("#filter-btn-mobile").click(function(e){
+  e.preventDefault();
+  $('body').addClass('lock');
+  $('.mobile-filter-menu').toggleClass('active');
+});
+
+$(".filter-title-main .close").click(function(e){
+  e.preventDefault();
+  $('body').removeClass('lock')
+  $('.mobile-filter-menu').removeClass('active');
+});
+
+$(".filter-block__item").click(function(e){
+  e.preventDefault();
+  $('.filter-body').addClass('lock');
+  $(this).next('.filter-block__wrapper').addClass('active');
+});
+
+
+$(".filter-block__title .back").click(function(e){
+  e.preventDefault();
+  $(this).closest(".filter-block__wrapper").removeClass('active');
+  $('.filter-body').removeClass('lock');
+});

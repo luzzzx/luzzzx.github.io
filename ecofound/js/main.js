814e9e6da450615dbf2b92a84ca8261b03b40be2
luzzzx <lu.vldmr@gmail.com>
2021-02-25 19:36:12 +0200

    eco
    eco

---
+++ ecofound/js/main.js
@@ -0,0 +1,69 @@
+$('.dropdown-menu li').on('click', function() {
+  var getValue = $(this).text();
+  $('.dropdown-select').text(getValue);
+});
+
+$('.slider-main').slick({
+  centerMode: true,
+  centerPadding: '32px',
+  slidesToShow: 1.65,
+  prevArrow: $('.product-slider__thumb-prev'),
+  nextArrow: $('.product-slider__thumb-next'),
+  responsive: [
+    {
+      breakpoint: 791,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+// $('.mobile-menu').on('click', function(e) {
+//   e.preventDefault();
+//   $(this).next('nav').slideToggle();
+// });
+
+// $(window).on("resize", function() {
+//   if($(this).width() > 1377) {
+//     $('.scroll-main').css('height', ($(this).height() - 222));
+//   } else{
+//     $('.scroll-main').css('height', '');
+//   }
+// }).trigger("resize");
+
+
+
+
+// progress bar
+new CircleProgress('.bar-circle', {
+	max: 100,
+	value: 80,
+  textFormat: 'none',
+});
+
+
+// counts progress
+var number = document.querySelector('.numberof'),
+		numberTop = number.getBoundingClientRect().top,
+    start = +number.innerHTML, end = +number.dataset.max;
+
+var interval = setInterval(function() {
+      number.innerHTML = ++start;
+      if(start == end) {
+          clearInterval(interval);
+      }
+  }, 5);
+

ac4def30ab242bb627baf73bf31be52b45240d96
luzzzx <lu.vldmr@gmail.com>
2021-04-05 19:34:48 +0300

    pages
    pages

--- fixme/js/main.js
+++ fixme/js/main.js
@@ -45,4 +45,36 @@ $(document).ready(function () {
         $('html').toggleClass('locked');
     });
     
+
+    $('.select-body__row-item').click(function(e) {
+        e.preventDefault();
+        let atr = $(this).attr('data-target');
+        $(atr).toggleClass('active');
+    });
+
+    $('.btn-model.back').click(function(e) {
+       let num = $(this).attr('data-close');
+       $(this).closest(`.${num}`).toggleClass('active');
+    });
+
+
+    // form app msg
+
+
+    
+    $('#order-cons').submit(function (e) { 
+        e.preventDefault();
+        $(".thx-msg").show().delay(2000).fadeOut();
+    });
+
+
+    // tabs label
+
+    $('.price-tab-body input:checkbox').change(function(){
+        if($(this).is(":checked")) {
+            $(this).parent().addClass("active");
+        } else {
+            $(this).parent().removeClass("active");
+        }
+    });
 });

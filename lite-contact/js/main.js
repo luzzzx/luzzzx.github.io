8ce692b3c7a834ca87d18a48bbf147d9f6372370
luzzzx <lu.vldmr@gmail.com>
2021-04-16 08:59:32 +0300

    header fix
    header fix

--- lite-contact/js/main.js
+++ lite-contact/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     $('.burger-btn').click(function(e) {
         e.preventDefault();
         $('.main-menu').toggleClass('active');
+        $('body').toggleClass('locked');
+        $('html').toggleClass('locked');
     });
 
 });

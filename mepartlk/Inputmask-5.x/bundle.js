b57926ce69c2d6282f1f767b962fcbab9760310f
luzzzx <lu.vldmr@gmail.com>
2021-05-07 11:48:11 +0300

    11
    1

--- mepartlk/Inputmask-5.x/bundle.js
+++
@@ -1,4 +0,0 @@
-require("./lib/extensions/inputmask.extensions");
-require("./lib/extensions/inputmask.date.extensions");
-require("./lib/extensions/inputmask.numeric.extensions");
-module.exports = require("./lib/inputmask.js");

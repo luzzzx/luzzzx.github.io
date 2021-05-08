f37010ce6f214fe0c79b6720a57c0e8aa45850c5
luzzzx <lu.vldmr@gmail.com>
2021-05-08 14:18:57 +0300

    modal+anketa
    modal+anketa

--- lite-contact/js/main.js
+++ lite-contact/js/main.js
@@ -8,4 +8,38 @@ $(document).ready(function () {
         $('header').toggleClass('active');
     });
 
+
+    function contractCheck() {
+        $('#contractCheck').change(function() {
+            if ($(this).is(':checked')){
+                console.log('hi')
+                $('.hidden-block').removeClass('blocked');
+                $('.contract-btn').removeClass('disabled');
+            } else {
+                $('.hidden-block').addClass('blocked');
+                $('.contract-btn').addClass('disabled');
+            }
+        });
+    }
+
+    contractCheck();
+
+    
+    // file upload 
+
+
+    $('.input-file').each(function() {
+          var $input = $(this),
+              $label = $input.next('.btn-upload'),
+              labelVal = $label.html();
+          
+        $input.on('change', function(element) {
+            console.log(element.target.value);
+            var fileName = '';
+            if (element.target.value) fileName = element.target.value.split('\\').pop();
+            fileName ? $label.addClass('has-file') : $label.removeClass('has-file').html(labelVal);
+         });
+    });
+
+    $('.contract-text').each(element, new SimpleBar());
 });

150f994b10fc2c60756db5a12838c3aee72bfab3
luzzzx <lu.vldmr@gmail.com>
2021-05-23 20:34:40 +0300

    1
    1

---
+++ landk/js/main.js
@@ -0,0 +1,59 @@
+"use strict"
+
+AOS.init();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
+    form.addEventListener('submit', formSend);
+    async function formSend(e) {
+        e.preventDefault();
+        let error = formValidate(form);
+
+        if (error === 0) {
+            console.log('lets go')
+        } else {
+            alert('Zapolni');
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false ) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+
+        }
+       return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error')
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error')
+    }
+
+    function emailTest(input) {
+        return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(input.value);
+    }
+
+});
